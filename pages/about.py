53beacccb803d41ee779c405cdff39f35ebee831
Mireya Isabel Rodríguez Pasillas <133801089+mirp01@users.noreply.github.com>
2023-10-27 13:45:45 +0000

    about

--- pages/about.py
+++ pages/about.py
@@ -1,7 +1,8 @@
 import streamlit as st
+from firebase_admin import firestore
 
 def app():
-    st.subheader('Pondering is a website created for users to')
+    st.subheader(st.session.username)
     st.subheader('share their valuable thoughts with the world.')
     st.markdown('Created by: [Ashwani Siwach](https://github.com/Ashwani132003)')
     st.markdown('Contact via mail: [siwachsahab1@gmail.com]')
